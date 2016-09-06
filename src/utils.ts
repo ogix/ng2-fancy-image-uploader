68639511387b3e2cb2d397da61cb21f93cba431a
Olegas <ogix777@gmail.com>
2016-09-06 12:31:25 +0300

    Fix indents

--- src/utils.ts
+++ src/utils.ts
@@ -1,58 +1,58 @@
 import {ResizeOptions} from './interfaces';
 
 export function createImage(url: string, cb: (i: HTMLImageElement) => void) {
-    var image = new Image();
-    image.onload = function () {
-        cb(image);
-    };
-    image.src = url;
+  var image = new Image();
+  image.onload = function () {
+    cb(image);
+  };
+  image.src = url;
 }
 
 const resizeAreaId = 'imageupload-resize-area';
 
 function getResizeArea() {
-    let resizeArea = document.getElementById(resizeAreaId);
-    if (!resizeArea) {
-        resizeArea = document.createElement('canvas');
-        resizeArea.id = resizeAreaId;
-        resizeArea.style.display = 'none';
-        document.body.appendChild(resizeArea);
-    }
+  let resizeArea = document.getElementById(resizeAreaId);
+  if (!resizeArea) {
+    resizeArea = document.createElement('canvas');
+    resizeArea.id = resizeAreaId;
+    resizeArea.style.display = 'none';
+    document.body.appendChild(resizeArea);
+  }
 
-    return <HTMLCanvasElement>resizeArea;
+  return <HTMLCanvasElement>resizeArea;
 }
 
 export function resizeImage(origImage: HTMLImageElement, {
-    resizeHeight,
-    resizeWidth,
-    resizeQuality = 0.7,
-    resizeType = 'image/jpeg'
-  }: ResizeOptions = {}) {
+  resizeHeight,
+  resizeWidth,
+  resizeQuality = 0.7,
+  resizeType = 'image/jpeg'
+}: ResizeOptions = {}) {
 
-    let canvas = getResizeArea();
+  let canvas = getResizeArea();
 
-    let height = origImage.height;
-    let width = origImage.width;
+  let height = origImage.height;
+  let width = origImage.width;
 
-    // calculate the width and height, constraining the proportions
-    if (width / height > resizeWidth / resizeHeight) {
-        width = Math.round(height * resizeWidth / resizeHeight);
-    } else {
-        height = Math.round(width * resizeHeight / resizeWidth);
-    }
+  // calculate the width and height, constraining the proportions
+  if (width / height > resizeWidth / resizeHeight) {
+    width = Math.round(height * resizeWidth / resizeHeight);
+  } else {
+    height = Math.round(width * resizeHeight / resizeWidth);
+  }
 
-    canvas.width = resizeWidth <= width ? resizeWidth : width;
-    canvas.height = resizeHeight <= height ? resizeHeight : height;
+  canvas.width = resizeWidth <= width ? resizeWidth : width;
+  canvas.height = resizeHeight <= height ? resizeHeight : height;
 
-    let offsetX = origImage.width / 2 - width / 2;
-    let offsetY = origImage.height / 2 - height / 2;
+  let offsetX = origImage.width / 2 - width / 2;
+  let offsetY = origImage.height / 2 - height / 2;
 
-    //draw image on canvas
-    const ctx = canvas.getContext("2d");
-    ctx.drawImage(origImage, offsetX, offsetY, width, height, 0, 0, canvas.width, canvas.height);
+  //draw image on canvas
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(origImage, offsetX, offsetY, width, height, 0, 0, canvas.width, canvas.height);
 
-    // get the data from canvas as 70% jpg (or specified type).
-    return canvas.toDataURL(resizeType, resizeQuality);
+  // get the data from canvas as 70% jpg (or specified type).
+  return canvas.toDataURL(resizeType, resizeQuality);
 }
 
 
